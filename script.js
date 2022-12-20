569ba6c89c735beb058001a2371ccb0eebd0e967
pankaj88279 <pankajjawi1993@gmail.com>
2022-12-20 13:09:09 +0530

    okkk

---
+++ script.js
@@ -0,0 +1,14 @@
+let i, j
+        for (i=0;i<=5;i++) {
+
+            console.log(i);
+            for (j=0;j<=i;j++) {
+                console.log(j);
+                
+                console.log("*")
+                
+                document.write("*");
+               
+            }
+            document.write("<br>");
+        }
